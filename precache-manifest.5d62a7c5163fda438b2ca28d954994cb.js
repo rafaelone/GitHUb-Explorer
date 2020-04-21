ef742dabe381c8cb2eddc79cacfa36f3dd0bde28
Rafael Sergio <rafa.sergio.02@live.com>
2020-04-21 15:52:13 -0300

    Updates

---
+++ precache-manifest.5d62a7c5163fda438b2ca28d954994cb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd8da285ce373e3f625183e77398b150",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f0cc56cd31ba37bd8f7f",
+    "url": "/static/js/2.dda5cb6c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.dda5cb6c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "778960bea538daee45d8",
+    "url": "/static/js/main.b45380af.chunk.js"
+  },
+  {
+    "revision": "f3c9e7b0288ca2c72147",
+    "url": "/static/js/runtime-main.3c0efd13.js"
+  },
+  {
+    "revision": "0b819d63338019a0f1cc7263e9c166ae",
+    "url": "/static/media/github_background.0b819d63.svg"
+  },
+  {
+    "revision": "04f6798f67fd4e48d053d05d18d9bd83",
+    "url": "/static/media/logo.04f6798f.svg"
+  }
+]);
